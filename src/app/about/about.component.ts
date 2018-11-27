63ae6579ea290fc6a54323d768ea6cc738c2e469
Your Name <abinayabtechit17@gmail.com>
2018-11-27 08:52:10 +0530

    UI update on all

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
+  about_data = [
+    {
+      'title': 'Exact Targeting',
+      'icon': 'assets/img/icon-1.png',
+      'desc': 'Advanced filters & Confidence Scores build powerful prospecting lists in seconds.'
+    },
+    {
+      'title': 'Quality Records',
+      'icon': 'assets/img/icon-2.png',
+      'desc': 'The Adapt community of 100,000+ users contribute millions of quality contacts.'
+    },
+    {
+      'title': 'Free Tools',
+      'icon': 'assets/img/icon-3.png',
+      'desc': 'The Adapt community of 100,000+ users contribute millions of quality contacts.'
+    },
+    {
+      'title': 'No Bouncing',
+      'icon': 'assets/img/icon-4.png',
+      'desc': 'The Adapt community of 100,000+ users contribute millions of quality contacts.'
+    }
+  ];
+
   constructor() { }
 
   ngOnInit() {
   }
 
 }
+
+
+
+
+
+
+
+
+
+
+
--- src/app/testimonial/testimonial.component.ts
+++ src/app/testimonial/testimonial.component.ts
@@ -8,25 +8,29 @@ import { Component, OnInit } from '@angular/core';
 export class TestimonialComponent implements OnInit {
 
   // tslint:disable-next-line:max-line-length
-  desc        = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum'
+  desc_1        = 'It has helped us tremendously with connecting to eCommerce champions who can get our nextgenparcel.com message to decision makers and reduce their shopping cart abandonment rates. I recommend it highly, thank you for such a useful tool!';
+  // tslint:disable-next-line:max-line-length
+  desc_2 = 'We have used adapt.io for some time now as our primary email finding tool and found it to be outstanding. It is very easy to use, have a user-friendly interface, is fast and most of all, it is accurate. We have tried other competitors in this market and have found adapt.io to be significantly better than the rest and we recommend it for any business that needs their services.';
+  // tslint:disable-next-line:max-line-length
+  desc_3 = 'Adapt is a very powerful tool, and their pricing is insanely good compared to other lead generation software platforms. The LinkedIn tool is very helpful, but the Google Sheets Addon is even better and so unique. Or lead gen and prospecting team absolutely love this tool.';
   testimonail = [
     {
       'name'       : 'Melanie ter Bong',
       'role'       : 'Partner and Co-Founder',
-      'description': this.desc,
+      'description': this.desc_1,
       'img'        : 'assets/img/test-1.png',
       'leaf'       : 'assets/img/test-11.png'},
     {
       'name'       : 'Sean Keough',
       'role'       : 'Executive Director',
-      'description': this.desc,
+      'description': this.desc_2,
       'img'        : 'assets/img/test-2.png',
       'leaf'       : 'assets/img/test-22.png'
     },
     {
       'name'       : 'Benjamin Westbroek',
       'role'       : 'Sales & Marketing Specialist',
-      'description': this.desc,
+      'description': this.desc_3,
       'img'        : 'assets/img/test-3.png',
       'leaf'       : 'assets/img/test-33.png'
     },
